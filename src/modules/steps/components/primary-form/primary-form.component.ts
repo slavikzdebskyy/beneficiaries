4b2ff07417a2f9428cd2f01bf9a99d6f5ad7ab96
slavikzdebskyy <zdebskyy.slavik@gmail.com>
2020-09-24 23:49:19 +0300

    make up form component / implement form group

--- src/modules/steps/components/primary-form/primary-form.component.ts
+++ src/modules/steps/components/primary-form/primary-form.component.ts
@@ -1,4 +1,10 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+export interface Relationship {
+  name: string;
+  value: boolean;
+}
 
 @Component({
   selector: 'ben-primary-form',
@@ -7,7 +13,31 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PrimaryFormComponent {
+  public form: FormGroup;
+  public minDate: Date;
+  public maxDate: Date;
+  public snn: string[];
+  public relationships: Relationship[];
 
   constructor() {
+    this.minDate = new Date(new Date().getFullYear() - 80, 0, 1);
+    this.maxDate = new Date(new Date().getFullYear() - 20, 11, 31);
+    this.snn = ['SNN'];
+    this.relationships = [{ name: 'Trust', value: true }];
+
+    this.initForm();
   }
+
+  private initForm(): void {
+    this.form = new FormGroup({
+      fullName: new FormControl('', [Validators.required]),
+      dateOfBirth: new FormControl('', [Validators.required]),
+      snn: new FormControl(this.snn[0], [Validators.required]),
+      optional: new FormControl(''),
+      relationship: new FormControl(this.relationships[0].value, [Validators.required]),
+      percentage: new FormControl('', [Validators.required]),
+    });
+  }
+
+
 }
--- src/modules/steps/steps.module.ts
+++ src/modules/steps/steps.module.ts
@@ -1,14 +1,34 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { StepsRoutingModule } from './steps-routing.module';
 import { PrimaryFormComponent } from './components';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+const MATERIAL_MODULES = [
+  MatFormFieldModule,
+  MatInputModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatSelectModule,
+  MatIconModule,
+];
 
 @NgModule({
   declarations: [PrimaryFormComponent],
   imports: [
+    ...MATERIAL_MODULES,
     CommonModule,
-    StepsRoutingModule
+    StepsRoutingModule,
+    ReactiveFormsModule,
+    MatButtonModule
   ]
 })
-export class StepsModule { }
+export class StepsModule {
+}
